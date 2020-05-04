d73c9734945aa536ac473305d462f8c397d83627
kamacizy <cancassim13@students.ecu.edu>
2020-05-03 21:33:23 -0400

    status codes and returns

--- api.py
+++ api.py
@@ -7,12 +7,31 @@ class API_Caller:
 
     def api_getter(self):
         res = requests.get(self.url,self.params)
-            
+        if res.status_code >= 200 and res.status_code < 300:
+            return res.json()
+        else:
+            return "Failed with status code: {0}".format(res.status_code)
+
     def api_poster(self):
-         res = requests.post(self.url,self.params)
+        res = requests.post(self.url,self.params)
+        if res.status_code >= 200 and res.status_code < 300:
+            return res.json()
+        else:
+            return "Failed with status code: {0}".format(res.status_code)
+
             
     def api_putter(self):
         res = requests.put(self.url,self.params)
+        if res.status_code >= 200 and res.status_code < 300:
+            return res.json()
+        else:
+            return "Failed with status code: {0}".format(res.status_code)
+
     
     def api_updater(self):
         res = requests.pa(self.url,self.params)
+        if res.status_code >= 200 and res.status_code < 300:
+            return res.json()
+        else:
+            return "Failed with status code: {0}".format(res.status_code)
+
